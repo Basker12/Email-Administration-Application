e6f92faeca414a33bc378ffb17ec5e4833b17586
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-03-17 19:53:13 +0000

    Update Email Administration Application.py

--- Email Administration Application.py
+++ Email Administration Application.py
@@ -1,5 +1,6 @@
 import random
 import string
+import sys
 
 Departments = {
     #Departments and there short names:
@@ -19,101 +20,110 @@ print("3: See your password and email" )
 print("-------------------------------")
 
 userInput = input("Write your choice: ")
+def email():
+    if userInput == "1":
+        userName = input("Write your name: ")
+        if userName.islower() or userName.isupper() or userName == userName:
+            newUserName = userName.title()
+            print("")
 
+        userSurname = input("Write your surname: ")
+        if userSurname.islower() or userSurname.isupper() or userSurname == userSurname:
+            newUserSurname = userSurname.title()
+            print("")
 
-if userInput == "1":
-    userName = input("Write your name: ")
-    if userName.islower() or userName.isupper() or userName == userName:
-        newUserName = userName.title()
+        userDepartment = input("Write your department in the company: ")
+        if userDepartment.islower() or userDepartment.upper() or userDepartment == userDepartment:
+            newUserDepartment = userDepartment.title()
+            print("")
+
+        userEmail = newUserName + newUserSurname + newUserDepartment
+
+        userGmail = "@gmail.com"
+        userYahoo = "@yahoo.com"
+        userIcloud = "@iCloud.com"
+        userOutlook = "@outlook.com"
+
+        print("-----------------")
+        print("1: " + userGmail)
+        print("2: " + userYahoo)
+        print("3: " + userIcloud)
+        print("4: " + userOutlook)
+        print("-----------------")
         print("")
 
-    userSurname = input("Write your surname: ")
-    if userSurname.islower() or userSurname.isupper() or userSurname == userSurname:
-        newUserSurname = userSurname.title()
+        userPick = input("Write your email ending choice: ")
+        if userPick == "1" or userPick == userGmail:
+            finalUserEmail = userEmail + userGmail
+
+        elif userPick == "2" or userPick == userYahoo:
+            finalUserEmail = userEmail + userYahoo
+
+        elif userPick == "3" or userPick == userIcloud:
+            finalUserEmail = userEmail + userIcloud
+
+        elif userPick == "4" or userPick == userOutlook:
+            finalUserEmail = userEmail + userOutlook
+
+        print("Your new email is: " + finalUserEmail)
         print("")
 
-    userDepartment = input("Write your department in the company: ")
-    if userDepartment.islower() or userDepartment.upper() or userDepartment == userDepartment:
-        newUserDepartment = userDepartment.title()
+
+        #Password with validation checks
+        randomNum = random.choice(string.ascii_letters)
+        randomNum2 = random.choice(string.ascii_letters)
+        randomNum3 = random.choice(string.ascii_letters)
+        randomNum4 = random.choice(string.ascii_letters)
+
+        randomNums = randomNum + randomNum2 + randomNum3 + randomNum4
+
+        userLength = False
+        while not userLength:
+            userPassword = input("Write a password: ")
+            if len(userPassword) >= 10:
+                userLength = True
+                print("That's a good password: " + userPassword)
+            else:
+                print("Your password isn't 10 characters long, please try again.")
+
+        if userPassword.isupper() or userPassword.islower() or userPassword == userPassword:
+            fUserPassword = userPassword.title()
+
+        finalUserPassword = fUserPassword + randomNums
+
+
+        print("This is your final password: " + finalUserPassword)
         print("")
+        print("This is your new email: " + finalUserEmail)
+        print("This is your password: " + finalUserPassword)
 
-    userEmail = newUserName + newUserSurname + newUserDepartment
+#global password
+#password = ""
 
-    userGmail = "@gmail.com"
-    userYahoo = "@yahoo.com"
-    userIcloud = "@iCloud.com"
-    userOutlook = "@outlook.com"
+def changePassword():
+    if userInput == "2":
 
-    print("-----------------")
-    print("1: " + userGmail)
-    print("2: " + userYahoo)
-    print("3: " + userIcloud)
-    print("4: " + userOutlook)
-    print("-----------------")
-    print("")
+        randomNum = random.choice(string.ascii_letters)
+        randomNum2 = random.choice(string.ascii_letters)
+        randomNum3 = random.choice(string.ascii_letters)
+        randomNum4 = random.choice(string.ascii_letters)
 
-    userPick = input("Write your email ending choice: ")
-    if userPick == "1" or userPick == userGmail:
-        finalUserEmail = userEmail + userGmail
+        randomNums = randomNum + randomNum2 + randomNum3 + randomNum4
 
-    elif userPick == "2" or userPick == userYahoo:
-        finalUserEmail = userEmail + userYahoo
+        print("Here you can change your password")
+        print("")
+        lastPass = input("Please write your last password: ")
+        if lastPass == password:
+            newPass = input("Write your new password: ")
+        elif lastPass != password:
+            print("Your password is incorrect")
+            sys.exit()
 
-    elif userPick == "3" or userPick == userIcloud:
-        finalUserEmail = userEmail + userIcloud
+        if newPass.islower() or newPass.upper() or newPass == newPass:
+            fNewPass = newPass.title()
 
-    elif userPick == "4" or userPick == userOutlook:
-        finalUserEmail = userEmail + userOutlook
+        finalNewPass = fNewPass + randomNums
+        print("This is your new password: " + finalNewPass)
 
-    print("Your new email is: " + finalUserEmail)
-    print("")
-
-
-    #Password with validation checks
-    randomNum = random.choice(string.ascii_letters)
-    randomNum2 = random.choice(string.ascii_letters)
-    randomNum3 = random.choice(string.ascii_letters)
-    randomNum4 = random.choice(string.ascii_letters)
-
-    randomNums = randomNum + randomNum2 + randomNum3 + randomNum4
-
-    userLength = False
-    while not userLength:
-        userPassword = input("Write a pass: ")
-        if len(userPassword) >= 10:
-            userLength = True
-            print("That's a good password: " + userPassword)
-        else:
-            print("Your password isn't 10 characters long, please try again.")
-
-    if userPassword.isupper() or userPassword.islower() or userPassword == userPassword:
-        fUserPassword = userPassword.title()
-
-    finalUserPassword = fUserPassword + randomNums
-    print("This is your final password: " + finalUserPassword)
-    print("")
-    print("This is your new email: " + finalUserEmail)
-    print("This is your password: " + finalUserPassword)
-
-elif userInput == "2":
-
-    randomNum = random.choice(string.ascii_letters)
-    randomNum2 = random.choice(string.ascii_letters)
-    randomNum3 = random.choice(string.ascii_letters)
-    randomNum4 = random.choice(string.ascii_letters)
-
-    randomNums = randomNum + randomNum2 + randomNum3 + randomNum4
-
-    print("Here you can change your password")
-    print("")
-    lastPass = input("Please write your last password: ")
-    if lastPass == lastPass:
-        newPass = input("Write your new password: ")
-    elif lastPass != "password":
-        print("Write your password is incorrect")
-
-    if newPass.islower() or newPass.upper() or newPass == newPass:
-        fNewPass = newPass.title()
-
-    finalNewPass = fNewPass + randomNums
-    print("This is your new password: " + fNewPass)
+email()
+changePassword()
